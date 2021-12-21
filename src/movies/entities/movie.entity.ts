1501d20145f12a0cb384838e9e7960e86fd02c92
Benny Hernandez <mail@bennyhernandez.com>
2021-12-21 15:47:00 -0500

    Feature: setup database for movies resource

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,43 @@
-export class Movie {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @Column()
+  adult: boolean;
+
+  @Column()
+  backdrop_path: string;
+
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  original_language: string;
+
+  @Column()
+  original_title: string;
+
+  @Column('text')
+  overview: string;
+
+  @Column()
+  popularity: number;
+
+  @Column()
+  poster_path: string;
+
+  @Column()
+  release_date: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  video: boolean;
+
+  @Column()
+  vote_average: number;
+
+  @Column()
+  vote_count: number;
+}
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MoviesRepository } from './movies.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MoviesRepository])],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
